bcf34906d9aa8c26fad47c5cf85c4b9df760a34f
Cameron Tong <cameron.tong2b@gmail.com>
2021-12-14 11:57:01 -0500

    refactor: replace regexp primitive with RegExp object

--- telephone-check.js
+++ telephone-check.js
@@ -1,6 +1,6 @@
 export function telephoneCheck(str) {
-  let validNum = /^(1\s?)?(\d{3}|\(\d{3}\))[\-\s]?\d{3}[\-\s]?\d{4}$/;
+  let validNum = new RegExp(
+    "^(1\\s?)?(\\d{3}|\\(\\d{3}\\))[-\\s]?\\d{3}[-\\s]?\\d{4}$"
+  );
   return validNum.test(str);
 }
-
-telephoneCheck("555-555-5555");
