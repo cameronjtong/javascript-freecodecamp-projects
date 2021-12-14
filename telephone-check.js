daa4b427fa2ad060f30d1733cbe664b42cae4898
Cameron Tong <cameron.tong2b@gmail.com>
2021-12-14 12:03:08 -0500

    refactor: comment regexp across multiple lines

--- telephone-check.js
+++ telephone-check.js
@@ -1,6 +1,13 @@
 export function telephoneCheck(str) {
   let validNum = new RegExp(
-    "^(1\\s?)?(\\d{3}|\\(\\d{3}\\))[-\\s]?\\d{3}[-\\s]?\\d{4}$"
+    [
+      "^(1\\s?)?", // optional country code
+      "(\\d{3}|\\(\\d{3}\\))", // 3-digit area code with optional parantheses
+      "[-\\s]?", // optional delimiter
+      "\\d{3}", // 3-digit exchange code
+      "[-\\s]?", // optional delimiter
+      "\\d{4}$", // 4-digit subscriber number
+    ].join("")
   );
   return validNum.test(str);
 }
